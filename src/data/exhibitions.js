77cff9c5d02643d3b472a775e9f79b3900927e56
amanda-yap <amandavyap@gmail.com>
2026-01-18 15:41:45 +1300

    make exhibition gallery with dummy data

---
+++ src/data/exhibitions.js
@@ -0,0 +1,38 @@
+export const exhibitions = [
+  {
+    slug: 'modern-visions',
+    title: 'Modern Visions',
+    location: 'Art Gallery',
+    description: 'A journey through contemporary art forms.',
+    date: 'January 2025',
+    images: [
+      { src: '/images/modern1.jpg', description: 'Oil paint' },
+      { src: '/images/modern2.jpg', description: 'Also oil paint' },
+      { src: '/images/modern3.jpg', description: 'Guess what this is' }
+    ],
+  },
+  {
+    slug: 'nature-in-focus',
+    title: 'Nature in Focus',
+    location: 'The Arts House Trust',
+    description: 'Exploring the beauty of natural landscapes.',
+    date: 'July 2025',
+    images: [
+      { src: '/images/nature1.jpg', description: 'Oil paint' },
+      { src: '/images/nature2.jpg', description: 'Also oil paint' },
+      { src: '/images/nature3.jpg', description: 'Guess what this is' }
+    ],
+  },
+  {
+    slug: 'urban-expressions',
+    title: 'Urban Expressions',
+    location: 'Art Stead',
+    description: 'Art inspired by city life.',
+    date: 'January 2026',
+    images: [
+      { src: '/images/urban1.jpg', description: 'Oil paint' },
+      { src: '/images/urban2.jpg', description: 'Also oil paint' },
+      { src: '/images/urban3.jpg', description: 'Guess what this is' }
+    ],
+  },
+];
