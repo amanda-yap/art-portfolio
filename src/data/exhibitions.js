a3b8be55c542a7f25107088697b75ca8fe840199
amanda-yap <amandavyap@gmail.com>
2026-01-22 15:17:48 +1300

    add client content

--- src/data/exhibitions.js
+++ src/data/exhibitions.js
@@ -1,49 +1,38 @@
-import modern1 from '../assets/modern1.jpg'
-import modern2 from '../assets/modern2.jpg'
-import modern3 from '../assets/modern3.jpg'
-import nature1 from '../assets/nature1.jpg'
-import nature2 from '../assets/nature2.jpg'
-import nature3 from '../assets/nature3.jpg'
-import urban1 from '../assets/urban1.jpg'
-import urban2 from '../assets/urban2.jpg'
-import urban3 from '../assets/urban3.jpg'
+import cat from '../assets/reciting-allegories/cat.jpg'
+import forest from '../assets/reciting-allegories/forest.jpg'
+import fox from '../assets/reciting-allegories/fox.jpg'
+import installation_view from '../assets/reciting-allegories/installation_view.jpg'
+import installation_view1 from '../assets/reciting-allegories/installation_view1.jpg'
+import rabbit from '../assets/reciting-allegories/rabbit.jpg'
+import rat from '../assets/reciting-allegories/rat.jpg'
+import serpent from '../assets/reciting-allegories/serpent.jpg'
+import stork from '../assets/reciting-allegories/stork.jpg'
+import wolf from '../assets/reciting-allegories/wolf.jpg'
 
 
 export const exhibitions = [
   {
-    slug: 'blue',
-    title: 'Blue',
-    location: 'Art Gallery',
-    description: 'Blue art.',
-    date: 'January 2025',
+    slug: 'reciting-allegories',
+    title: 'Reciting Allegories',
+    location: 'Elam School of Fine Arts',
+    description: ['This series of paintings recontextualises fables and oral traditions from a contemporary perspective. I draw inspiration from Aesop\'s Fables, Fairy Tales, Nursery Tales, Oral Traditions and similar stories. I explore how they have been depicted and retold, with stories such as Little Red Riding Hood, Puss in Boots, The Fox and the Rabbit, and similar tales.',
+    'Through research, I identified recurring tropes that appear across these stories, focusing on how they are depicted as animals and environments to convey deeper meanings. I transformed these tropes into painting, translating their meanings into my own work.',
+    'Through the process of painting, I worked with dyes and oil pastels, allowing the inherent entropy of the dyes to influence the outcome while contrasting the structure provided by the oil pastels. The dye bleeds into my canvas, shaping my tropes while the oil pastels define them. This practice embodies my illustrative, loose, and unfinished style of painting, reflecting the indeterminacy of oral traditions and allowing viewers to assign their own meaning.',
+    'My canvases come in the form of bedsheets I have gathered, imitating the act of gathering stories. I explore materiality through painting onto the bedsheets to connect conceptually to the moment when these stories are told. I dive deeper into the relationship between bedsheets and the dreaming state we associate with stories.',
+    'My intention in these artworks is to establish and build on the whakapapa of the stories I pull inspiration from, by engaging with the concepts embedded within these stories from a historical perspective. I want the viewer to engage with the art through their own interpretations, establishing their own meaning from the art to build upon the existing ideas.'],
+    date: '29 November - Sunday 30 November 2025',
+    cover_image: {src: installation_view, description: ''},
     images: [
-      { src: modern1, description: 'Oil paint' },
-      { src: modern2, description: 'Also oil paint' },
-      { src: modern3, description: 'Guess what this is' }
+      { src: stork, description: 'Stork (2025), 2610 x 1850, Dye and Oil Pastels' },
+      { src: serpent, description: 'Serpent (2025), 2480 x 2600, Dye and Oil Pastels' },
+      { src: wolf, description: 'Wolf (2025), 2270 x 2600, Dye and Oil Pastels' },
+      { src: forest, description: 'Forest (2025), 2750 x 2400, Dye and Oil Pastels' },
+      { src: cat, description: 'Cat (2025), 2460 x 2100, Dye and Oil Pastels' },
+      { src: rat, description: 'Rat (2025), 2550 x 1700, Dye and Oil Pastels' },
+      { src: rabbit, description: 'Rabbit (2025), 2480 x 1900, Dye and Oil Pastels' },
+      { src: fox, description: 'Fox (2025), 2460 x 1200, Dye and Oil Pastel' },
+      { src: installation_view, description: '' },
+      { src: installation_view1, description: '' }
     ],
-  },
-  {
-    slug: 'nature-in-focus',
-    title: 'Nature in Focus',
-    location: 'The Arts House Trust',
-    description: 'Exploring the beauty of natural landscapes.',
-    date: 'July 2025',
-    images: [
-      { src: nature1, description: 'Oil paint' },
-      { src: nature2, description: 'Also oil paint' },
-      { src: nature3, description: 'Guess what this is' }
-    ],
-  },
-  {
-    slug: 'vast-lands',
-    title: 'Vast Lands',
-    location: 'Art Stead',
-    description: 'Portraying the vast plains through paint.',
-    date: 'January 2026',
-    images: [
-      { src: urban1, description: 'Oil paint' },
-      { src: urban2, description: 'Also oil paint' },
-      { src: urban3, description: 'Guess what this is' }
-    ],
-  },
+  }
 ];
